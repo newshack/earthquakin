11193dda7ea4dfc41e2a7de8c66d190b99c7f307
newshack <davidwatts@dyn-160-39-175-224.dyn.columbia.edu>
2014-06-09 13:03:46 -0400

    leanred a bit about functions

--- earthquake.py
+++ earthquake.py
@@ -1,1 +1,16 @@
-print "hello world"
+
+# make a function called greeting that asks for your name
+# def command creates a function
+def greeting():
+	name = raw_input("what is your name? ")
+	print "Hello world, " + name
+
+def hello(name):
+	print "Hello, " + name
+
+def add(a, b):
+	return a + b
+
+hello("Alex")
+
+print add(2, 3) + add(4, 5)
